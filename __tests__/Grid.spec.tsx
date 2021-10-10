6815ccfc1d00a6c1a80c9e3deb099705795cd5c0
Liszt <lin.liszt@gmail.com>
2021-10-10 23:49:03 +0800

    feature / Add Grid component, which used to show number / flag / mine
    - Implement a component *Grid* used to show number / flag / mine
    - Apply *identity-obj-proxy* for component style's unit test
    - Add unit test for *Grid* component
    - Remove template files

---
+++ __tests__/Grid.spec.tsx
@@ -0,0 +1,78 @@
+/**
+ * @jest-environment jsdom
+ */
+import '@testing-library/jest-dom'
+import { fireEvent, render } from '@testing-library/react'
+import React from 'react'
+
+import Grid from '../src/components/Grid'
+
+describe('Grid component props', () => {
+  test('should render without content', () => {
+    const { container } = render(<Grid />)
+    expect(container!.firstChild?.textContent).toBe('')
+  })
+
+  test('should render with passing className', () => {
+    const customClass: string = 'customClass'
+    const { container } = render(<Grid className={customClass} />)
+    expect(container!.firstChild).toHaveClass('grid', customClass)
+  })
+
+  test('should render with className "grid--pressed" when active', () => {
+    const { container } = render(<Grid isActive />)
+    expect(container!.firstChild).toHaveClass('grid--pressed')
+  })
+
+  test('should render without number when inactive', () => {
+    const { container } = render(<Grid number={1} />)
+    expect(container!.firstChild?.textContent).toBe('')
+  })
+
+  test('should render with number when active', () => {
+    const { container } = render(<Grid number={1} isActive />)
+    expect(container!.firstChild?.textContent).toBe('1')
+  })
+
+  test('should render with className "grid--marked" only when marked', () => {
+    const { container } = render(<Grid />)
+    fireEvent.contextMenu(container!.firstChild)
+    expect(container?.firstChild).toHaveClass('grid', 'grid--marked')
+
+    fireEvent.contextMenu(container!.firstChild)
+    expect(container?.firstChild).not.toHaveClass('grid--marked')
+  })
+
+  test('should render with className "grid--mine" when both isMine and showMine are true', () => {
+    const { container } = render(<Grid isMine showMine />)
+    expect(container?.firstChild).toHaveClass('grid', 'grid--mine')
+  })
+
+  test('should trigger event "onClick"', () => {
+    const handleClick = jest.fn()
+    const { container } = render(<Grid onClick={handleClick} />)
+    fireEvent.click(container!.firstChild)
+    expect(handleClick).toHaveBeenCalledTimes(1)
+  })
+
+  test('should trigger event "onClick" only when inactive', () => {
+    const handleClick = jest.fn()
+    const { container } = render(<Grid onClick={handleClick} isActive />)
+    fireEvent.click(container!.firstChild)
+    expect(handleClick).toHaveBeenCalledTimes(0)
+  })
+
+  test('should trigger event "onMarked" if defined', () => {
+    const handleMarked = jest.fn()
+    const { container } = render(<Grid onMarked={handleMarked} />)
+    fireEvent.contextMenu(container!.firstChild)
+    expect(handleMarked).toHaveBeenCalledTimes(1)
+  })
+
+  test('should trigger event "onMarked" only when inactive', () => {
+    const handleMarked = jest.fn()
+    const { container } = render(<Grid onMarked={handleMarked} isActive />)
+    fireEvent.contextMenu(container!.firstChild)
+    expect(handleMarked).toHaveBeenCalledTimes(0)
+  })
+})
---
+++ src/components/Grid.tsx
@@ -0,0 +1,72 @@
+import React, { useState } from 'react'
+import PropTypes from 'prop-types'
+import clsx from 'clsx'
+import GridStyle from 'scss/components/grid.scss'
+
+const GridProps = {
+  className: PropTypes.string,
+  number: PropTypes.number,
+  isActive: PropTypes.bool,
+  isMine: PropTypes.bool,
+  showMine: PropTypes.bool,
+  onClick: PropTypes.func,
+  onMarked: PropTypes.func,
+}
+
+const Grid = ({
+  className, number, isActive, isMine, showMine, onClick, onMarked,
+}: PropTypes.InferProps<typeof GridProps>) => {
+  const [marked, setMarked] = useState(false)
+
+  const gridClass: string = clsx(
+    GridStyle.grid, {
+      [GridStyle['grid--pressed']]: isActive,
+      [GridStyle['grid--marked']]: marked,
+      [GridStyle['grid--mine']]: showMine && isMine,
+      [GridStyle['grid--variant1']]: number! <= 2,
+      [GridStyle['grid--variant2']]: number! > 2 && number! <= 4,
+      [GridStyle['grid--variant3']]: number! > 4 && number! <= 6,
+      [GridStyle['grid--variant4']]: number! > 6 && number! <= 8,
+    },
+    className,
+  )
+
+  const handleContextMenu = (e: Event) => {
+    e.stopPropagation()
+    e.preventDefault()
+
+    if (isActive) {
+      return
+    }
+
+    setMarked(!marked)
+    if (onMarked) {
+      onMarked(!marked, e)
+    }
+  }
+
+  return (
+    <button
+      type="button"
+      className={gridClass}
+      onClick={(!isActive && onClick) ? onClick : undefined}
+      onContextMenu={handleContextMenu}
+    >
+      { (isActive && !isMine) ? (number || '') : '' }
+    </button>
+  )
+}
+
+Grid.propTypes = GridProps
+
+Grid.defaultProps = {
+  className: '',
+  number: undefined,
+  isActive: false,
+  isMine: false,
+  showMine: false,
+  onClick: undefined,
+  onMarked: undefined,
+}
+
+export default Grid
