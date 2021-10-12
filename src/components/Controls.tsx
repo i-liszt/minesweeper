f66ecd3e385ff88a197a03ae42d088d9af0ee7d7
Liszt <lin.liszt@gmail.com>
2021-10-12 23:01:08 +0800

    add Control component composed by Timer and Flag

--- src/components/Controls.tsx
+++ src/components/Controls.tsx
@@ -1,9 +1,37 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import clsx from 'clsx'
 import ControlsStyle from 'scss/components/controls.scss'
+import Timer from '~/components/Timer'
+import Flag from '~/components/Flag'
+// import { Level } from '~/constants/level'
 
-export default () => (
-  <div className={ControlsStyle.controls}>
-    <div>Time</div>
-    <div className={ControlsStyle.controls__mines}>Input Mine</div>
+const ControlsProps = {
+  className: PropTypes.string,
+  // level: PropTypes.oneOf(Object.values(Level)),
+  seconds: PropTypes.number,
+  remainFlags: PropTypes.number,
+  running: PropTypes.bool,
+  onTimeChange: PropTypes.func,
+}
+
+const Controls = ({
+  className, seconds, remainFlags, running, onTimeChange,
+}: PropTypes.InferProps<typeof ControlsProps>) => (
+  <div className={clsx(ControlsStyle.controls, className)}>
+    <Timer seconds={seconds} running={running} onChange={onTimeChange} />
+    <Flag className={ControlsStyle.controls__flags} remainFlags={remainFlags} />
   </div>
 )
+
+Controls.propTypes = ControlsProps
+Controls.defaultProps = {
+  className: undefined,
+  // level: Level.Easy,
+  seconds: 0,
+  remainFlags: 0,
+  running: false,
+  onTimeChange: undefined,
+}
+
+export default Controls
