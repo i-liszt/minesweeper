be7b876e50e648f815ace1e50ad73d3786db5a97
Liszt <lin.liszt@gmail.com>
2021-10-16 01:22:45 +0800

    amend the solution to match the exact question

--- __tests__/utils/count7s.spec.ts
+++ __tests__/utils/count7s.spec.ts
@@ -1,23 +1,23 @@
-import count7s from '~/utils/count7s'
+import { count7s } from '~/utils/count7s'
 
 describe('Count 7s', () => {
-  test('should 7 occurs 1 time in the range 1~7', () => {
+  test('should 7 appears in 1 number of the range 1~7', () => {
     expect(count7s(7)).toBe(1)
   })
 
-  test('should 7 occurs 2 times in the range 1~20', () => {
+  test('should 7 appears in 2 numbers of the range 1~20', () => {
     expect(count7s(20)).toBe(2)
   })
 
-  test('should 7 occurs 8 times in the range 1~70', () => {
+  test('should 7 appears in 8 numbers of the range 1~70', () => {
     expect(count7s(70)).toBe(8)
   })
 
-  test('should 7 occurs 19 times in the range 1~100', () => {
+  test('should 7 appears in 19 numbers of the range 1~100', () => {
     expect(count7s(100)).toBe(19)
   })
 
-  test('should 7 occurs 300 times in the range 1~1000', () => {
-    expect(count7s(1000)).toBe(300)
+  test('should 7 appears in 271 numbers of the range 1~1000', () => {
+    expect(count7s(1000)).toBe(271)
   })
 })
--- src/utils/count7s.ts
+++ src/utils/count7s.ts
@@ -2,24 +2,38 @@ export const count7sByRegex = (end: number) => {
   let count: number = 0
 
   for (let i = 1; i <= end; i++) {
-    count += (String(i).match(/7/g) || []).length
+    count += /7/g.test(String(i)) ? 1 : 0
   }
 
   return count
 }
 
-export const count7s = (end: number) => {
-  let count: number = 0
-
-  for (let i = 1; i <= end; i++) {
-    // eslint-disable-next-line @typescript-eslint/no-loop-func
-    String(i).split('').forEach((digit: string) => {
-      if (digit === '7') {
-        count++
-      }
-    })
+export const count7s = (end: number): number => {
+  if (!end || end < 7) {
+    return 0
   }
-  return count
-}
 
-export default count7s
+  const digits: number = Math.floor(Math.log10(end))
+  const resultsOf10e = new Array(digits + 1)
+  resultsOf10e[0] = 0
+  resultsOf10e[1] = 1
+
+  for (let i = 2; i <= digits; i++) {
+    resultsOf10e[i] = resultsOf10e[i - 1] * 9 + (10 ** (i - 1))
+  }
+
+  const pow = 10 ** digits
+  if (end === pow) {
+    return resultsOf10e[digits]
+  }
+
+  const mostSignificantDigit = Math.floor(end / pow)
+  if (mostSignificantDigit > 7) {
+    return (mostSignificantDigit - 1) * resultsOf10e[digits] + pow + count7s(end % pow)
+  } if (mostSignificantDigit === 7) {
+    // +1 is for (mostSignificantDigit * pow), e.g., 70.
+    return mostSignificantDigit * resultsOf10e[digits] + (end % pow) + 1
+  }
+
+  return mostSignificantDigit * resultsOf10e[digits] + count7s(end % pow)
+}
