df7b1cfdaf85d42984a4b9533c1ef684e8e2167f
Liszt <lin.liszt@gmail.com>
2021-10-15 10:41:52 +0800

    fix / modify the win role to prevent a user puts flags to all grids
    - As title. Make the win rule strict

--- src/utils/game.ts
+++ src/utils/game.ts
@@ -1,6 +1,7 @@
 export const isLose = (clickedGrid: GridData): boolean => clickedGrid.isMine && clickedGrid.explored
 
 // eslint-disable-next-line arrow-body-style
-export const isWin = (minesList: GridData[]): boolean => {
-  return minesList.every((grid) => grid.isMine && grid.flagged)
+export const isWin = (minesList: GridData[], remainFlags: number): boolean => {
+  return remainFlags === 0
+    && minesList.every((grid) => grid.isMine && grid.flagged)
 }
